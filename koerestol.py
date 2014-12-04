4c441cee041b8a2e591d476eb37dd0e487dad57e
Kristian Mide <fas@mide.dk>
2014-12-04 23:28:44 +0100

    Fixing up signedness

--- koerestol.py
+++ koerestol.py
@@ -1,33 +1,38 @@
 import serial
 import sys
-
+import struct
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 255, 0, 255])
+plt.axis([-127, 127, -127, 127])
 plt.ion()
 plt.show()
-
-while True: 
+itr = 0;
+while True:
+	
 	if ord(ser.read()) == 74:
 			x = False
 			y = False
 			for i in xrange(0, 10):
 				if i is 2:
-					x = ord(ser.read())
-					sys.stdout.write(str(x))
-					sys.stdout.write("\t")
+					x = struct.unpack('b', ser.read())[0]
+#					x = ord(ser.read())
+#					sys.stdout.write(str(x))
+#					sys.stdout.write("\t")
 					continue	
 				if i is 3:
-					y = ord(ser.read())
-					sys.stdout.write(str(y))
-					sys.stdout.write("\t")
+					y = struct.unpack('b', ser.read())[0]
+#					y = ord(ser.read())
+#					sys.stdout.write(str(y))
+#					sys.stdout.write("\t")
 					continue
-				if x and y:
-					plt.scatter(x, y)
-					plt.draw()
-				sys.stdout.write(str(ord(ser.read())))
-				sys.stdout.write("\t")
+				ser.read()
+			sys.stdout.write("x: %s, y: %s" % (str(x), str(y)))
+			itr = itr + 1
+			if itr % 15 is 1:			
+				print "draw! %d" % itr
+				plt.scatter(x, y)
+				plt.draw()
 
 			print(" ")
