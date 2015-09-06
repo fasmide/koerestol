b2e84c6c188e0d093784c4fd857b7dd9d33712e1
Kristian Mide <fas@mide.dk>
2015-09-06 21:43:26 +0200

    Even better better_test.py

--- ps3tests/better_test.py
+++ ps3tests/better_test.py
@@ -7,8 +7,11 @@ for dev in devices:
 
 index = int(raw_input('Choose index: ') or 0)
 
-codes_we_dont_want_to_see = [0, 4, 5, 6];
+codes_we_dont_want_to_see = [4, 5, 6]; #These are accelerometer
 
 for event in devices[index].read_loop():
+	if event.code == 0 and event.type == 0 and event.value == 0:
+		continue
+
 	if event.code not in codes_we_dont_want_to_see:
 		print(event)
